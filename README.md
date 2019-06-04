9270a92ec750052524370df360a067158c16e518
gabyfulchic <gaby.fulchic@ynov.com>
2019-06-04 17:19:16 +0200

    init the repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# BackupBashScript
+
+```
+Little script with some variables to automate the backup of paths in linux with bash.  
+You should change the variables to adapt the script for any project.  
+``` 
+
+```
+You can use cron to execute it every night for example.  
+> crontab -l  
+30 20 * * * /srv/scripts/backup.sh  
+
+If you're searching how to make cron, check that website :  
+[crontab.guru](crontab.guru)  
+``` 
---
+++ backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#      Made By FULCHIC Gaby       #
+#          To Practice            #
+
+# Backup Location
+
+backupPlace="/srv/backup"
+sudo mkdir -p $backupPlace
+
+if [ $? == 1 ]
+then
+    rm -rf $backupPlace
+    sudo mkdir -p $backupPlace
+fi
+
+numberOfFiles="$(ls -1 | wc -l)"
+
+if [ $numberOfFiles -ge "4" ]
+then
+    rm "$(ls -t | tail -1)"
+    create_backupfile
+elif [ $numberOfFiles -lt "4" ]
+then
+    create_backupfile
+fi
+
+create_backupfile {
+
+  tar -czvf "$backupPlace/sharemybox01_$(date +%Y-%m-%d).tar.gz" \
+  	/etc/apache2/ \
+  	/etc/php/ \
+  	/home/ \
+  	/etc/mysql/ \
+  	/etc/freerad/ \
+  	/etc/hosts \
+  	/etc/hostname \
+  	/etc/resolv.conf \
+}
+
+# For futures features
+# BDD Backups
