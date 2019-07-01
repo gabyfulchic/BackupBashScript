3774ddb8078d922f4bcc455db7c91830064209f5
gabyf <gaby.fulchic@ynov.com>
2019-07-01 17:06:57 +0200

    Added backup mysql

--- backup.sh
+++ backup.sh
@@ -5,27 +5,44 @@
 
 # Backup Location
 
-backupPlace="/srv/backup"
+backupPlace="/srv/backup/general"
+sqlDumpPlace="/srv/backup/sql"
+
 sudo mkdir -p $backupPlace
-
-create_backupfile () {
-
-  tar -czvf "$backupPlace/sharemybox01_$(date +%Y-%m-%d).tar.gz" \
-  	/etc/apache2/ \
-  	/etc/php/ \
-  	/home/ \
-  	/etc/mysql/ \
-  	/etc/freerad/ \
-  	/etc/hosts \
-  	/etc/hostname \
-  	/etc/resolv.conf \
-}
 
 if [ $? == 1 ]
 then
     rm -rf $backupPlace
     sudo mkdir -p $backupPlace
 fi
+
+sudo mkdir -p $sqlDumpPlace
+
+if [ $? == 1 ]
+then
+    rm -rf $sqlDumpPlace
+    sudo mkdir -p $sqlDumpPlace
+fi
+
+## FUTURE FEATURE
+## BDD Backups
+## Add the path to the tar -czvf
+# DBUser="root"
+# DBPasswd="root"
+# DBBase="database"
+# mysqldump -u $DBUser -p$DBPasswd $DBBase > "$sqlDumpPlace/sqlBackup.sql"
+
+create_backupfile () {
+  tar -czvf "$backupPlace/sharemybox01_$(date +%Y-%m-%d).tar.gz" \
+  	/etc/apache2/ \
+  	/etc/php/ \
+  	/home/ \
+  	/etc/freerad/ \
+  	/etc/hosts \
+  	/etc/hostname \
+  	/etc/resolv.conf
+#   /srv/backup/sql/ \
+}
 
 numberOfFiles="$(ls -1 | wc -l)"
 
@@ -37,6 +54,3 @@ elif [ $numberOfFiles -lt "4" ]
 then
     create_backupfile
 fi
-
-# For futures features
-# BDD Backups
